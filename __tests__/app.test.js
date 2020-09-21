02eed17be457c18d30821034e127aa95d7c2f94d
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-21 16:17:32 -0700

    add GET / route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
+describe('server', () => {
+  it('checks that plain text is returned with the word hi after making a GET request to /', async() => {
+    const response = await request(app)
+      .get('/');
 
+    expect(response.text).toEqual('hi');
+  });
 });
